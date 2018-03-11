c42e85f0b5b26ce402cd7b8f3b9f8d822332a048
Jon Schoning <jonschoning@gmail.com>
2018-03-10 23:47:36 -0600

    star/unstar

--- src/Handler/Edit.hs
+++ src/Handler/Edit.hs
@@ -6,6 +6,8 @@ import Database.Persist.Sql
 
 import Import
 
+-- routes
+
 deleteDeleteR :: Int64 -> Handler Html
 deleteDeleteR bid = do
   userId <- requireAuthId
@@ -24,7 +26,22 @@ postReadR bid = do
     update k_bid [BookmarkToRead =. False]
   pure ""
 
+postStarR :: Int64 -> Handler Html
+postStarR bid = _setSelected bid True
+
+postUnstarR :: Int64 -> Handler Html
+postUnstarR bid = _setSelected bid False
+
 -- common
+
+_setSelected :: Int64 -> Bool -> Handler Html
+_setSelected bid selected = do
+  userId <- requireAuthId
+  runDB $ do
+    let k_bid = toSqlKey bid
+    bm <- requireResource userId k_bid
+    update k_bid [BookmarkSelected =. selected]
+  pure ""
 
 requireResource :: UserId -> Key Bookmark -> DBM Handler Bookmark
 requireResource userId k_bid =
